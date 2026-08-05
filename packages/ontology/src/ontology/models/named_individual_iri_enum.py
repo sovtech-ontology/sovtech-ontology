999f15381220926cf2ab0f1a7c0d4abac5823494
kaninsin <reedishigamiwayne00@gmail.com>
2026-08-04 22:43:47 -0400

    feat: add named-individual enum factory

---
+++ packages/ontology/src/ontology/models/named_individual_iri_enum.py
@@ -0,0 +1,11 @@
+from ontology.models.iri_enum import IriEnum
+from ontology.models.named_individuals import named_individuals
+from ontology.models.namespaces import SVTO_CBOX
+
+
+def named_individual_iri_enum(type_: str) -> IriEnum:
+    """The "@id" constraint for a type's named individuals, for use in a
+    metadata slot: Annotated[AnyUrl, named_individual_iri_enum("X")]."""
+    return IriEnum(
+        tuple(f"{SVTO_CBOX}{key}{type_}" for key in named_individuals[type_])
+    )
