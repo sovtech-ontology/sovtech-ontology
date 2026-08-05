99755f0f86e601f722e4991892691ffe4aafdc09
kaninsin <reedishigamiwayne00@gmail.com>
2026-08-04 21:21:56 -0400

    feat: add config to ontology

---
+++ packages/ontology/src/ontology/models/config.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from pydantic import FilePath
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+CONFIG_DIRECTORY_PATH = Path(__file__).parent.parent.parent.absolute()
+DATA_DIRECTORY_PATH = Path(__file__).parent.parent.absolute() / "data"
+INPUT_DATA_DIRECTORY_PATH = DATA_DIRECTORY_PATH / "input"
+
+
+class Config(BaseSettings):
+    """Pydantic BaseSettings to store config variables."""
+
+    sovtech_jsonld_context: FilePath = (
+        INPUT_DATA_DIRECTORY_PATH / "sovtech_jsonld_context.json"
+    )
+
+    model_config = SettingsConfigDict(
+        env_file=(
+            CONFIG_DIRECTORY_PATH / ".env.local",
+            CONFIG_DIRECTORY_PATH / ".env.secret",
+        ),
+        extra="ignore",
+        env_file_encoding="utf-8",
+        validate_default=False,
+    )
