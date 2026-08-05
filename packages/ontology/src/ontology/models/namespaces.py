7f1f24c65609848e968b2c0505fd8def2c9f3ff2
kaninsin <reedishigamiwayne00@gmail.com>
2026-08-04 21:29:03 -0400

    feat: add ontology namespaces

---
+++ packages/ontology/src/ontology/models/namespaces.py
@@ -0,0 +1,11 @@
+"""Namespace IRIs for the SovTech ontology."""
+
+SCHEMA = "https://schema.org/"
+
+SKOS = "http://www.w3.org/2004/02/skos/core#"
+
+SVTO = "https://purl.org/svto/ontology#"
+
+SVTO_CBOX = "https://purl.org/svto/cbox#"
+
+SVTO_DATA = "https://purl.org/svto/data/"
